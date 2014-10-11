39c7c70333e2a61ab0e85a088fa873c9fcf90f5b
hhimanshu <harit.himanshu@gmail.com>
2014-10-11 14:57:16 -0700

    getUsersByState with tests

--- persistence/src/main/java/com/learner/persistence/UserQueries.java
+++ persistence/src/main/java/com/learner/persistence/UserQueries.java
@@ -1,15 +1,18 @@
 package com.learner.persistence;
 
 import com.learner.persistence.configuration.CrudService;
+import com.learner.persistence.entities.QAddress;
 import com.learner.persistence.entities.QUser;
 import com.learner.persistence.entities.User;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
+import java.util.List;
 
 public class UserQueries {
 	private static final QUser user = QUser.user;
+	private static final QAddress address = QAddress.address;
 	private final CrudService crudService;
 
 	@Inject
@@ -20,5 +23,10 @@ public class UserQueries {
 	@Nullable
 	public User getUserById(@Nonnull final String userId) {
 		return crudService.query(user).where(user.id.eq(userId)).uniqueResult(user);
+	}
+
+	@Nonnull
+	public List<User> getUserByState(@Nonnull final String state) {
+		return crudService.query(user).innerJoin(user.addresses, address).where(address.state.eq(state)).list(user);
 	}
 }
--- persistence/src/test/java/com/learner/persistence/UserQueriesTest.java
+++ persistence/src/test/java/com/learner/persistence/UserQueriesTest.java
@@ -1,8 +1,11 @@
 package com.learner.persistence;
 
 import com.learner.persistence.entities.AbstractUnitTest;
+import com.learner.persistence.entities.Address;
 import com.learner.persistence.entities.User;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -19,5 +22,68 @@ public class UserQueriesTest extends AbstractUnitTest {
 		final User userByQuery = new UserQueries(crudService).getUserById(user.getId());
 		assertNotNull(userByQuery);
 		assertEquals(user, userByQuery);
+	}
+
+	@Test
+	public void testGetUserByState() throws Exception {
+		final User billGates;
+		{
+			final Address billGatesAddress;
+			{
+				billGatesAddress = new Address("One Microsoft Way", null, "Redmond", "Washington", 98052, "USA");
+				crudService.create(billGatesAddress);
+				jpaRule.changeTransaction();
+			}
+			{
+				billGates = crudService.create(new User("Steve", "Jobs"));
+				billGates.addAddress(billGatesAddress);
+				jpaRule.changeTransaction();
+			}
+		}
+
+		final User steveJobs;
+		{
+			final Address steveJobsAddress;
+			{
+				steveJobsAddress = new Address("1 infinite loop", null, "Cupertino", "California", 95014, "USA");
+				crudService.create(steveJobsAddress);
+				jpaRule.changeTransaction();
+			}
+			{
+				steveJobs = crudService.create(new User("Steve", "Jobs"));
+				steveJobs.addAddress(steveJobsAddress);
+				jpaRule.changeTransaction();
+			}
+		}
+
+		final User elonMusk;
+		{
+			final Address elonMuskAddress;
+			{
+				elonMuskAddress = new Address("45500 Fremont Blvd", null, "Fremont", "California", 94538, "USA");
+				crudService.create(elonMuskAddress);
+				jpaRule.changeTransaction();
+			}
+			{
+				elonMusk = crudService.create(new User("Elon", "Musk"));
+				elonMusk.addAddress(elonMuskAddress);
+				jpaRule.changeTransaction();
+			}
+
+		}
+
+		{
+			final List<User> californiaUsers = new UserQueries(crudService).getUserByState("California");
+			assertNotNull(californiaUsers);
+			assertEquals("Two users must be found in CA", 2, californiaUsers.size());
+			assertEquals(steveJobs, californiaUsers.get(0));
+			assertEquals(elonMusk, californiaUsers.get(1));
+		}
+		{
+			final List<User> washingtonUsers = new UserQueries(crudService).getUserByState("Washington");
+			assertNotNull(washingtonUsers);
+			assertEquals("1 user must be found in WA", 1, washingtonUsers.size());
+			assertEquals(billGates, washingtonUsers.get(0));
+		}
 	}
 }
