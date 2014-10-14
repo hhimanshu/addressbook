7f185334423a3d51007a2c4c6177daeab3f3f44c
hhimanshu <harit.himanshu@gmail.com>
2014-10-13 21:42:39 -0700

    completed integration tests

--- integration/src/test/java/com/learner/integration/TestUser.java
+++ integration/src/test/java/com/learner/integration/TestUser.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Category(Integration.class)
 public class TestUser {
@@ -27,7 +28,43 @@ public class TestUser {
                 .accept(MediaType.APPLICATION_JSON).get();
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         final JsonNode responsePayload = new ObjectMapper().readTree(response.readEntity(String.class));
-        System.out.println(responsePayload.toString());
+
         assertEquals(1, responsePayload.size());
+        assertTrue(responsePayload.get(0).has("id"));
+        assertTrue(responsePayload.get(0).has("firstName"));
+        assertTrue(responsePayload.get(0).has("lastName"));
+        assertEquals("The user must be Steve Jobs", "Steve", responsePayload.get(0).get("firstName").asText());
+        assertEquals("The user must be Steve Jobs", "Jobs", responsePayload.get(0).get("lastName").asText());
+
+        assertTrue(responsePayload.get(0).has("nickName"));
+        assertTrue(responsePayload.get(0).has("email"));
+        assertTrue(responsePayload.get(0).has("phonePresentations"));
+        assertEquals("2 Phones are present", 2, responsePayload.get(0).get("phonePresentations").size());
+        assertTrue(responsePayload.get(0).has("addressPresentations"));
+        assertEquals("1 address is present", 1, responsePayload.get(0).get("addressPresentations").size());
+    }
+
+    @Test
+    public void testGetUsersByPhoneAreaCode() throws IOException {
+        final Client client = ClientBuilder.newClient();
+        final WebTarget webTarget = client.target("http://localhost:9090/addressBook/rest/users/areaCode/202");
+        final Response response = webTarget
+                .request(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON).get();
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        final JsonNode responsePayload = new ObjectMapper().readTree(response.readEntity(String.class));
+
+        assertEquals(1, responsePayload.size());
+        assertTrue(responsePayload.get(0).has("id"));
+        assertTrue(responsePayload.get(0).has("firstName"));
+        assertEquals("The user must be Bill Gates", "Bill", responsePayload.get(0).get("firstName").asText());
+        assertEquals("The user must be Bill Gates", "Gates", responsePayload.get(0).get("lastName").asText());
+        assertTrue(responsePayload.get(0).has("lastName"));
+        assertTrue(responsePayload.get(0).has("nickName"));
+        assertTrue(responsePayload.get(0).has("email"));
+        assertTrue(responsePayload.get(0).has("phonePresentations"));
+        assertEquals("2 Phones are present", 1, responsePayload.get(0).get("phonePresentations").size());
+        assertTrue(responsePayload.get(0).has("addressPresentations"));
+        assertEquals("1 address is present", 1, responsePayload.get(0).get("addressPresentations").size());
     }
 }
